aa1d18ad6f32935e0d655a9b6fc8946b00ebb87d
Anish092 <37304018+Anish092@users.noreply.github.com>
2018-09-14 10:11:29 -0400

    secomnf

--- WeirdCalc.java
+++ WeirdCalc.java
@@ -5,15 +5,17 @@ public class WeirdCalc{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		float num;
-		float num2;
+		float num, num2, num3;
+		float ave;
 		num = 1;
-	    num2 = 1;
+		num2 = 1;
+
+
 
 		Scanner keyboard = new Scanner(System.in);
 		System.out.print("Please enter first number: ");
 		try{
-		    num = keyboard.nextInt();
+		    num = keyboard.nextFloat();
 		}
 		catch(NumberFormatException ex){
 
@@ -21,7 +23,7 @@ public class WeirdCalc{
 
 		System.out.print("Please enter second number: ");
 		try{
-		    num2 = keyboard.nextInt();
+		    num2 = keyboard.nextFloat();
 		}
 		catch(NumberFormatException ex){
 
@@ -29,31 +31,31 @@ public class WeirdCalc{
 
 
 		System.out.printf("%50s", "Sum:");
-		System.out.printf("%24.2f", num + num2);
+		System.out.printf("%24.0f", num + num2);
 		System.out.println();
 
 		System.out.printf("%50s", "Difference:");
-		System.out.printf("%24.2f", num - num2);
+		System.out.printf("%24.0f", num - num2);
 		System.out.println();
 
 		System.out.printf("%50s", "Product:");
-		System.out.printf("%24.2f", num * num2);
+		System.out.printf("%24.0f", num * num2);
 		System.out.println();
 
 		System.out.printf("%50s", "Average:");
-		System.out.printf("%24.2f", (num + num2)/2);
+		System.out.printf("%27.2f", ((num + num2)/2));
 		System.out.println();
 
 		System.out.printf("%50s", "Difference:");
-		System.out.printf("%24.2f", Math.abs(num - num2));
+		System.out.printf("%24.0f", Math.abs(num - num2));
 		System.out.println();
 
 		System.out.printf("%50s", "Maximum:");
-		System.out.printf("%24.2f", Math.max(num, num2));
+		System.out.printf("%24.0f", Math.max(num, num2));
 		System.out.println();
 
 		System.out.printf("%50s", "Minimum:");
-		System.out.printf("%24.2f", Math.min(num, num2));
+		System.out.printf("%24.0f", Math.min(num, num2));
 		System.out.println();
 
 
